5551ef3ebcc7f2c1defb277c9fe6195e5aed710c
pedrolucas7i <145859069+pedrolucas7i@users.noreply.github.com>
2024-08-26 16:57:12 +0100

    #1 first commit

---
+++ static/loadingArtigos.js
@@ -0,0 +1,18 @@
+let load_screen = `
+<div style="top: ${window.scrollY}px" id="loader-back">
+      <div id="loader">
+        <img src="../../IMG/desktop.webp" alt="" class="center-image" />
+        <img src="../../IMG/90YV0GB2-M0NA10.jpg" alt="" class="orbiting-image" />
+        <img src="../../IMG/zen3-r55500mpk.webp" alt="" class="orbiting-image" />
+        <img src="../../IMG/motherboard.jpeg" alt="" class="orbiting-image" />
+      </div>
+    </div>
+`;
+
+document.write(load_screen);
+
+window.addEventListener("load", function () {
+  // Oculte a loading screen
+  var loadingScreen = document.getElementById("loader-back");
+  loadingScreen.style.display = "none";
+});
